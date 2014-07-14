0424e92731adc72ee277f94d56e8c3b25a9ea647
hkbarton <hkbarton@gmail.com>
2014-07-14 16:54:18 -0700

    update

--- boxtowers.js
+++ boxtowers.js
@@ -44,9 +44,9 @@ function processInput(input){
       }
       if (v1>=1 && v1<=100 && v2>=1 && v2<=100 && v3>=1 && v3<=100){
         // push data format: [box_id, width, length, height]
-        data.push([i, Math.min(v1,v2), Math.max(v1,v2), v3]);
-        data.push([i, Math.min(v1,v3), Math.max(v1,v3), v2]);
-        data.push([i, Math.min(v2,v3), Math.max(v2,v3), v1]);
+        data.push([i-1, Math.min(v1,v2), Math.max(v1,v2), v3]);
+        data.push([i-1, Math.min(v1,v3), Math.max(v1,v3), v2]);
+        data.push([i-1, Math.min(v2,v3), Math.max(v2,v3), v1]);
       }else{
         throw "Input line " + i + " have invalidate data, numbers should between 1 and 100.";
       }
@@ -60,7 +60,7 @@ function processInput(input){
 function calulate(data){
   // sort by base area
   data.sort(function(a, b){
-    return b[1]*b[2] - a[1]*a[1];
+    return b[1]*b[2] - a[1]*a[2];
   });
   // solve problem using DP
   // h(i) = height of box i
@@ -69,41 +69,51 @@ function calulate(data){
   // start DP using bottom-up, no recursion
   var result = Array(data.length);
   var usedBox = Array(data.length);
+  var usedBoxIndex = Array(data.length);
   var max = 0;
-  var boxIDs = [];
+  var maxidx = 0;
   var i = 0;
-  var j = 0;
   for (i=0;i<data.length;i++){
     max = 0;
-    result[i] = {};
+    maxidx = 0;
     usedBox[i] = [];
-    boxIDs = [];
-    for (j=0;j<i && (data[j][1]>=data[i][1] && data[j][2]>=data[i][2]);j++){
-      if (result[j].value > max){
-        max = result[j].value;
-        boxIDs = usedBox[j].slice();
+    usedBoxIndex[i] = [];
+    for (var j=0;j<i && 
+      (data[j][1]>=data[i][1] && data[j][2]>=data[i][2]);
+      j++){
+      if (result[j] > max){
+        max = result[j];
+        maxidx = j;
       }
     }
-    for(j=0;j<boxIDs.length;j++) usedBox[i].push(boxIDs[j]);
-    usedBox[i].push(data[i][0]);
-    result[i].value = max + data[i][3];
-    console.log(usedBox[i]);
-    console.log(result[i].value);
-    // check if we use same box twice in this solution
-    result[i].useDuplicateBox = false;
-    usedBox[i].sort();
-    for (var k=0;i<usedBox[i].length-1;k++){
-      if (usedBox[i][k]==usedBox[i][k+1]){
-        result[i].useDuplicateBox = true;
-        break;
-      }
+    usedBox[i] = usedBox[maxidx].concat([data[i][0]]);
+    usedBoxIndex[i] = usedBoxIndex[maxidx].concat([i]);
+    result[i] = max + data[i][3];
+  }
+  max = 0;
+  maxidx = 0;
+  for (i=0;i<data.length;i++){
+    if (result[i]>max){
+      max = result[i];
+      maxidx = i;
+    }
+  }
+  // recaulate for remove duplicate box
+  max = 0;
+  var boxMax = Array(data.length/3);
+  var boxIndex = 0;
+  var dataIndex = 0;
+  for (i=0;i<usedBox[maxidx].length;i++){
+    boxIndex = usedBox[maxidx][i];
+    dataIndex = usedBoxIndex[maxidx][i]; 
+    if (boxMax[boxIndex]===undefined) boxMax[boxIndex] = 0;
+    if (boxMax[boxIndex] < data[dataIndex][3]){
+      boxMax[boxIndex] = data[dataIndex][3];
     }
   }
   max = 0;
-  for (i=0;i<data.length;i++){
-    if (result[i].value>max && result[i].useDuplicateBox===false){
-      max = result[i].value;
-    }
+  for(i=0;i<boxMax.length;i++){
+    max += boxMax[i];
   }
   console.log(max);
 }
